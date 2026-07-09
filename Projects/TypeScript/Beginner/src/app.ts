b7bcdbf72451ab210d416b34616741c817bcd505
mikee <mitiou00@gmail.com>
2026-07-09 13:54:38 -0700

    chore: Learning how to call and use functions with loops, and built in Math functions

--- Projects/TypeScript/Beginner/src/app.ts
+++ Projects/TypeScript/Beginner/src/app.ts
@@ -1,19 +1,15 @@
-const movieTitle: string = 'A New Hope';
-
-var Fruit:string = 'apple';
-    
-
-switch (Fruit) {
-    case 'orange':
-        console.log(`this orange`);
-        break;
-    case 'apple':
-        console.log(`this apple`);
-        break;
-    case 'banana':
-        console.log(`this bananas`)
-        break;
-
+function GetReview(title: string): string|number {
+    if (title=='A New Hope') {
+        return 'An instant Classic!';
+    }
+    else {
+        return Math.floor(Math.random() * 10);
+    }
 }
 
+const movieTitle: string = 'A New Hope';
+
+let movieReview: string|number = GetReview(movieTitle);
+
 console.log(`Movie title: ${movieTitle}`);
+console.log(`${movieReview}`);
