e0d16aace9068ef59c699f5cc95c32ac61373bca
mikee <mitiou00@gmail.com>
2026-07-09 13:47:52 -0700

    chore: writing, learning TypeScript loops and coniditions

--- Projects/TypeScript/Beginner/src/app.ts
+++ Projects/TypeScript/Beginner/src/app.ts
@@ -1,3 +1,19 @@
 const movieTitle: string = 'A New Hope';
 
+var Fruit:string = 'apple';
+    
+
+switch (Fruit) {
+    case 'orange':
+        console.log(`this orange`);
+        break;
+    case 'apple':
+        console.log(`this apple`);
+        break;
+    case 'banana':
+        console.log(`this bananas`)
+        break;
+
+}
+
 console.log(`Movie title: ${movieTitle}`);
