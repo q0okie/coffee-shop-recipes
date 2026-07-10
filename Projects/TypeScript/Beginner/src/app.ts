2b2005693ef1f906cbbfbf99f4d6b075213bb71d
mikee <mitiou00@gmail.com>
2026-07-10 09:23:37 -0700

    chore: Exploring TypeScript functions, variable declarations and their types

--- Projects/TypeScript/Beginner/src/app.ts
+++ Projects/TypeScript/Beginner/src/app.ts
@@ -1,15 +1,19 @@
-function GetReview(title: string): string|number {
-    if (title=='A New Hope') {
-        return 'An instant Classic!';
-    }
-    else {
-        return Math.floor(Math.random() * 10);
-    }
-}
+
+
+
+function createCustomer(name:string, age?:number) { };
+
+function getBookTitle(title:string = 'The C Programming Language') { };
+
+function reccBookTitle(title:string = mostPopularBook) { };
+
+var mostPopularBook:string;
+
+function getReview(a:string) {};
 
 const movieTitle: string = 'A New Hope';
 
-let movieReview: string|number = GetReview(movieTitle);
+let movieReview: string|number = getReview(movieTitle);
 
 console.log(`Movie title: ${movieTitle}`);
 console.log(`${movieReview}`);
